65cd7fb86fb420b6f3b6900c4916acdb339e5bac
Piotr Figiel <figiel+github@gmail.com>
2012-10-08 21:11:26 +0200

    working smoothly, on init get all data, no filtering yet

--- nowreadingengine.cpp
+++ nowreadingengine.cpp
@@ -4,13 +4,14 @@
 
 #include <QtCore/QDir>
 #include <QtCore/QFile>
+#include <QtCore/QScopedPointer>
 #include <QtCore/QStringList>
 #include <QtCore/QVariant>
 #include <QtCore/QXmlStreamReader>
-
-#include <syslog.h>
+#include <poppler-qt4.h>
 
 #include <iostream>
+#include <syslog.h>
 
 using namespace std;
 
@@ -27,6 +28,53 @@ struct NowReadingEntry {
     uint totalPages;
 };
 
+class PagesNumberProvider {
+public:
+    virtual bool getNumberOfPages(uint &pageNumberOut) = 0;
+    virtual ~PagesNumberProvider() { }
+};
+
+class PDFPagesNumberProvider : public PagesNumberProvider {
+public:
+    PDFPagesNumberProvider(const QString &filePath)
+        : m_filePath(filePath)
+    {
+        static PopplerInitializer pInit;
+    }
+
+    bool getNumberOfPages(uint &pageNumberOut)
+    {
+        QScopedPointer<Poppler::Document> document(Poppler::Document::load(m_filePath));
+
+        if (!document)
+            return false;
+
+        pageNumberOut = document->numPages();
+
+        return true;
+    }
+    class PopplerInitializer {
+    public:
+        static void quietPoppler(const QString &, const QVariant &) { }
+        PopplerInitializer() {
+            Poppler::setDebugErrorFunction(quietPoppler, 0);
+        }
+    };
+
+private:
+    QString m_filePath;
+};
+
+class PagesNumberProviderFactory {
+public:
+    static PagesNumberProvider *createPagesNumberProviderByFileName(const QString &fileName)
+    {
+        if (fileName.endsWith(".pdf", Qt::CaseInsensitive))
+            return new PDFPagesNumberProvider(fileName);
+
+        return NULL;
+    }
+};
 
 bool convertOkularXMLFileToEntry(const QString &okularFilePath, NowReadingEntry &outEntry)
 {
@@ -38,6 +86,8 @@ bool convertOkularXMLFileToEntry(const QString &okularFilePath, NowReadingEntry 
 
     QXmlStreamReader xml(&okularFile);
     QString vp;
+
+    outEntry.path = QString();
 
     while (!xml.atEnd()) {
         if (QXmlStreamReader::StartElement != xml.readNext())
@@ -51,8 +101,6 @@ bool convertOkularXMLFileToEntry(const QString &okularFilePath, NowReadingEntry 
         }
     }
 
-    /* get PDF file path: */
-
     if (outEntry.path.isEmpty())
         return false;
 
@@ -62,9 +110,18 @@ bool convertOkularXMLFileToEntry(const QString &okularFilePath, NowReadingEntry 
         return false;
 
     bool pageOk = true;
-    outEntry.currentPage = tokens.first().toUInt(&pageOk);
+    outEntry.currentPage = tokens.first().toUInt(&pageOk) + 1 /* the value in vp is 0-based */;
 
     if (!pageOk)
+        return false;
+
+    QScopedPointer<PagesNumberProvider> pageNumberProvider
+            (PagesNumberProviderFactory::createPagesNumberProviderByFileName(outEntry.path));
+
+    if (!pageNumberProvider)
+        return false;
+
+    if (!pageNumberProvider->getNumberOfPages(outEntry.totalPages))
         return false;
 
     return true;
@@ -88,13 +145,16 @@ QStringList getOkularXMLFiles()
 }
 
 int main(int argc, char **argv) {
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
+
     QStringList okularFiles = getOkularXMLFiles();
     QStringList::const_iterator it = okularFiles.constBegin();
 
     for ( ; it != okularFiles.constEnd(); ++it) {
         NowReadingEntry entry;
         if (convertOkularXMLFileToEntry(*it, entry)) {
-            cout << entry.path.toStdString() << ":" << entry.currentPage << endl;
+            cout << entry.path.toStdString() << ":" << entry.currentPage << "/" << entry.totalPages << endl;
         }
     }
 
@@ -103,7 +163,16 @@ int main(int argc, char **argv) {
 
 void NowReadingEngine::init()
 {
-    syslog(LOG_ALERT, "I'm started! :)");
+    QStringList okularFiles = getOkularXMLFiles();
+    QStringList::const_iterator it = okularFiles.constBegin();
+
+    for ( ; it != okularFiles.constEnd(); ++it) {
+        NowReadingEntry entry;
+        if (convertOkularXMLFileToEntry(*it, entry)) {
+            setData(entry.path, "currentPage", entry.currentPage);
+            setData(entry.path, "totalPages", entry.totalPages);
+        }
+    }
 }
 
 K_EXPORT_PLASMA_DATAENGINE(nowreading, NowReadingEngine)
